a95e53be0cf20bb0fe492bc4aac34c2023c881a3
Zerling14 <Zerling14@gmail.com>
2017-05-21 09:02:32 +0700

    Fix mode_cell

--- src/cell.c
+++ src/cell.c
@@ -3,11 +3,7 @@
 void mode_cell(Cell *cell, int mode, char ch)
 {
 	cell->state = mode;
-	if(mode) {
-		cell->c = ch;
-	} else {
-		cell->c = ch;
-	}
+	cell->c = ch;
 }
 
 int check_cell(Cell *cell)
